852f0a80ee88789ef35e35b243f9644d44dfb938
Wojciech Gryglas <wgryglas@meil.pw.edu.pl>
2019-10-12 18:45:20 +0200

    Supprting named screenshot regions

---
+++ dialogs.py
@@ -0,0 +1,35 @@
+
+from pyqode.qt.QtCore import Signal, Qt
+from pyqode.qt.QtWidgets import QDialog, QPushButton, QStyle, QVBoxLayout, QHBoxLayout
+
+class OKCancelDialog(QDialog):
+    on_ok = Signal()
+    on_close = Signal()
+
+    def __init__(self, centeralWidget):
+        QDialog.__init__(self)
+
+        ok = QPushButton()
+        ok.setIcon(self.style().standardIcon(QStyle.SP_DialogOkButton))
+        ok.setToolTip('Ok')
+        ok.clicked.connect(self.on_ok.emit)
+        ok.clicked.connect(lambda: self.close())
+
+        cancel = QPushButton()
+        cancel.setIcon(self.style().standardIcon(QStyle.SP_DialogCancelButton))
+        ok.setToolTip('Cancel')
+        cancel.clicked.connect(lambda: self.close())
+
+        buttonsLt = QHBoxLayout()
+        buttonsLt.addStretch()
+        buttonsLt.addWidget(ok)
+        buttonsLt.addWidget(cancel)
+
+        mainLt = QVBoxLayout()
+        mainLt.addWidget(centeralWidget, Qt.AlignCenter)
+        mainLt.addLayout(buttonsLt)
+
+        self.setLayout(mainLt)
+
+    def closeEvent(self, QCloseEvent):
+        self.on_close.emit()
--- test.py
+++ test.py
@@ -96,9 +96,11 @@ from core import Session
 #     sys.exit(app.exec_())
 
 import sys
-from PyQt4 import QtGui, QtCore
-from PyQt4.QtCore import Qt, QRect, QPoint
-from PyQt4.QtGui import QPushButton, QStyle, QHBoxLayout, QVBoxLayout, QWidget, QCursor, QApplication
+from pyqode.qt.QtCore import Qt, QRect, QPoint, Signal, QTimer, QRegExp
+from pyqode.qt.QtGui import QMainWindow, QPushButton, QStyle, QHBoxLayout, QVBoxLayout, QWidget, QCursor, QLabel, \
+    QLineEdit, QIntValidator, QApplication, QPainter, QColor, QPen, QComboBox, QDialog, QRegExpValidator
+from dialogs import OKCancelDialog
+import icons
 
 class Direction:
     left = 0
@@ -115,21 +117,68 @@ def makeHCenterLayout(widget):
     return lt
 
 
-class WindowRegionSelector(QtGui.QMainWindow):
-    def __init__(self):
-        super(WindowRegionSelector, self).__init__()
-        self.initUI()
+class WindowRegionSelector(QMainWindow):
+    on_quit = Signal()
+    on_take_screenshot = Signal(int, int, int, int)
 
+    def __init__(self, regions=dict(), gridSpcing=5, useGrid=True):
+        super(WindowRegionSelector, self).__init__()
         self.curr_dir = None
         self.press_pos = QPoint()
         self.startRect = QRect()
-        self.useGrid = True
-        self.gridPixelSize = 10
+        self.useGrid = useGrid
+        self.gridPixelSize = gridSpcing
+        self.savedRegions = regions
+
+        self.maximizeButton = QPushButton('Maximize')
+        self.maximizeButton.clicked.connect(self.toggleFullScreen)
+
+        def makeEdit(value):
+            edit = QLineEdit(value)
+            edit.returnPressed.connect(self.updateRectFromText)
+            v = QIntValidator()
+            v.setBottom(0)
+            edit.setValidator(v)
+            return edit
+
+        self.xEdit = makeEdit('100')
+        self.yEdit = makeEdit('100')
+        self.wEdit = makeEdit('200')
+        self.hEdit = makeEdit('100')
+        self.editWidget = QWidget()
+
+        self.takeScreenshot = QPushButton()
+        self.takeScreenshot.setIcon(icons.get('screenshot'))
+        self.takeScreenshot.setToolTip('Take screenshot now')
+        self.takeScreenshot.clicked.connect(lambda: self.doScreenShot(0))
+
+        delay = 5
+        self.takeScreenshotDelayed = QPushButton()
+        self.takeScreenshotDelayed.setIcon(icons.get('screenshot'))
+        self.takeScreenshotDelayed.setToolTip('Take screenshot in {} seconds'.format(delay))
+        self.takeScreenshotDelayed.setText('{} s'.format(delay))
+        self.takeScreenshotDelayed.clicked.connect(lambda: self.doScreenShot(delay))
+
+        self.namedRegionsSelector = QComboBox()
+        self.namedRegionsSelector.addItem('Custom')
+        for name in self.savedRegions:
+            self.namedRegionsSelector.addItem(name)
+        self.namedRegionsSelector.addItem('Save Current')
+        self.namedRegionsSelector.currentIndexChanged.connect(self.handleNamedRegionSelection)
+        self.initUI()
+
+        self.updateTextFromRect()
+
+    def doScreenShot(self, delay=0):
+        r = self.currentRect()
+        if delay == 0:
+            self.on_take_screenshot.emit(r.x(), r.y(), r.width(), r.height())
+        else:
+            QTimer.singleShot(1000*delay, lambda: self.on_take_screenshot.emit(r.x(), r.y(), r.width(), r.height()))
 
     def grapMousePosAndRect(self):
         p = QCursor.pos()
         self.press_pos = p
-        print self.pos()
         self.startRect.setRect(self.pos().x(), self.pos().y(), self.width(), self.height())
 
     def mouseReleaseEvent(self, QMouseEvent):
@@ -137,6 +186,49 @@ class WindowRegionSelector(QtGui.QMainWindow):
 
     def releaseDir(self):
         self.curr_dir = None
+
+    def currentRect(self):
+        return QRect(int(self.xEdit.text()), int(self.yEdit.text()),
+                     int(self.wEdit.text()), int(self.hEdit.text()))
+
+    def updateRectFromText(self):
+        currR = self.rect()
+        r = self.currentRect()
+
+        if currR.width() != r.width() or currR.height() != r.height():
+            self.resize(r.width(), r.height())
+
+        if currR.x() != r.x() or currR.y() != r.y():
+            self.move(r.x(), r.y())
+
+        self.checkRegionNameIfMatch(r)
+
+    def updateTextFromRect(self):
+        p = self.pos()
+        s = self.size()
+        r = QRect(p.x(), p.y(), s.width(), s.height())
+        self.xEdit.setText(str(r.x()))
+        self.yEdit.setText(str(r.y()))
+        self.wEdit.setText(str(r.width()))
+        self.hEdit.setText(str(r.height()))
+        self.checkRegionNameIfMatch(r)
+
+    def checkRegionNameIfMatch(self, r):
+        print 'checking'
+        for name in self.savedRegions:
+            reg = self.savedRegions[name]
+            if reg == r:
+                index = self.namedRegionsSelector.findText(name)
+                print 'matching', name, index
+                if -1 < index != self.namedRegionsSelector.currentIndex():
+                    self.namedRegionsSelector.setCurrentIndex(index)
+                return
+            else:
+                print reg, r
+
+        if self.namedRegionsSelector.currentIndex() != 0:
+            self.namedRegionsSelector.setCurrentIndex(0)
+
 
     def snapCoordinate(self, c):
         rest = c % self.gridPixelSize
@@ -179,35 +271,149 @@ class WindowRegionSelector(QtGui.QMainWindow):
             return
 
         result = self.constraint(result)
-        self.resize(result.width(), result.height())
-        self.move(result.x(), result.y())
 
-    def makeFullScreen(self):
-        # self.window().setWindowFlags(Qt.WindowMaximized)
-        self.window().showMaximized()
+        self.xEdit.setText(str(result.x()))
+        self.yEdit.setText(str(result.y()))
+        self.wEdit.setText(str(result.width()))
+        self.hEdit.setText(str(result.height()))
+        self.updateRectFromText()
+
+    def toggleFullScreen(self):
+        if self.window().isMaximized():
+            self.window().showNormal()
+            self.maximizeButton.setText('Full Screen')
+            self.editWidget.setVisible(True)
+            # self.updateTextFromRect()
+        else:
+            self.window().showMaximized()
+            self.maximizeButton.setText('Part of Screen')
+            self.editWidget.setVisible(False)
 
     def paintEvent(self, e):
-        qp = QtGui.QPainter()
+        qp = QPainter()
         qp.begin(self)
-        qp.setPen(QtGui.QColor(255, 255, 255))
-        qp.drawRect(QRect(0, 0, self.width()-1, self.height()-1))
+
+        qp.setPen(QColor(0, 0, 0))
+        qp.drawRect(QRect(0, 0, self.width() - 1, self.height() - 1))
+
+        qp.setPen(QPen(QColor(255, 255, 255), 1, Qt.DashLine))
+        # qp.setPen(QColor(255, 255, 255))
+        qp.drawRect(QRect(0, 0, self.width() - 1, self.height() - 1))
+
         qp.end()
 
-    def setPos(self, dir):
-        print 'setting dir', dir
-        self.curr_dir = dir
+    def handleNamedRegionSelection(self, index):
+        if index == 0:
+            return
+        elif self.namedRegionsSelector.itemText(index) == 'Save Current':
+            self.namedRegionsSelector.setCurrentIndex(0)
+            self.openSaveRegionDialog()
+        else:
+            name = self.namedRegionsSelector.itemText(index)
+            r = self.savedRegions[name]
+            print 'loading', r
+            self.resize(r.width(), r.height())
+            self.move(r.x(), r.y())
+            self.updateTextFromRect()
 
-    def makeDirButton(self, dir, iconId, style):
+    def savedCurrentRegion(self, name):
+        r = self.currentRect()
+        self.savedRegions[name] = r
+        self.namedRegionsSelector.insertItem(1, name)
+
+    def openSaveRegionDialog(self):
+        nameInput = QLineEdit()
+        regex = '[A-Za-z0-9_]+'
+
+        validator = QRegExpValidator(QRegExp(regex))
+        nameInput.setValidator(validator)
+
+        widget = QWidget()
+        lt = QHBoxLayout()
+        lt.addStretch()
+        lt.addWidget(QLabel('Name'))
+        lt.addWidget(nameInput)
+        lt.addStretch()
+        widget.setLayout(lt)
+
+        d = OKCancelDialog(widget)
+        d.setWindowTitle('Save Region as ...')
+        d.setModal(True)
+        d.showNormal()
+
+        d.on_ok.connect(lambda: self.savedCurrentRegion(nameInput.text()))
+
+
+    def setPos(self, direction):
+        self.curr_dir = direction
+
+    def makeDirButton(self, direction, iconId, style):
         b = QPushButton()
         if style:
             b.setStyleSheet(style)
-        b.pressed.connect(lambda: self.setPos(dir))
+        b.pressed.connect(lambda: self.setPos(direction))
         b.pressed.connect(self.grapMousePosAndRect)
         b.setIcon(self.style().standardIcon(iconId))
         return b
 
+    def layoutEdits(self):
+        def setSizes(widget):
+            widget.setMaximumWidth(40)
+            widget.setMaximumHeight(30)
+
+        setSizes(self.xEdit)
+        setSizes(self.yEdit)
+        setSizes(self.wEdit)
+        setSizes(self.hEdit)
+
+        pos = QHBoxLayout()
+        pos.setSpacing(0)
+        pos.addWidget(QLabel('x'))
+        pos.addSpacing(0)
+        pos.addWidget(self.xEdit)
+        pos.addSpacing(20)
+        pos.addWidget(QLabel('y'))
+        pos.addSpacing(0)
+        pos.addWidget(self.yEdit)
+
+        size = QHBoxLayout()
+        size.setSpacing(0)
+        size.setAlignment(Qt.AlignCenter)
+        size.addWidget(QLabel('w'))
+        size.addWidget(self.wEdit)
+        size.addSpacing(20)
+        size.addWidget(QLabel('h'))
+        size.addWidget(self.hEdit)
+
+        lt = QVBoxLayout()
+        lt.addLayout(pos)
+        lt.addLayout(size)
+
+        self.editWidget.setStyleSheet("""
+        QLabel {
+            background: rgba(140, 180, 255, 127);;
+            padding: 0;
+            margin: 0;
+        }
+        QLineEdit {
+            background: rgba(140, 180, 255, 127);;
+            border:none;
+            padding: 0;
+            margin: 0;
+        }
+        QLineEdit:focus {
+            background: rgba(140, 180, 255, 200);;
+            border:none;
+            padding: 0;
+            margin: 0;
+        }
+        
+        """)
+        self.editWidget.setLayout(lt)
+        self.editWidget.setMaximumWidth(300)
+
     def initUI(self):
-        transp_button_style = """
+        dir_button_style = """
         QPushButton {
           margin: 0;
           padding: 0;
@@ -220,27 +426,69 @@ class WindowRegionSelector(QtGui.QMainWindow):
             border-radius:3px;
         }
         """
+        in_button_style = """
+        QPushButton {
+          margin: 5px;
+          padding: 3px;
+          background:rgba(140, 180, 255, 127);
+          border:none;
+          color: black;
+          border-radius:3px;
+        }
+        QPushButton:hover {
+            border: 1px solid rgb(100, 140, 200);
+            background:rgba(140, 180, 255, 200);
+        }
+        QComboBox {
+          margin: 5px;
+          padding: 3px;
+          background:rgba(140, 180, 255, 127);
+          border:none;
+          color: black;
+          border-radius:3px;
+        }
+        QComboBox:hover {
+            border: 1px solid rgb(100, 140, 200);
+            background:rgba(140, 180, 255, 200);
+        }
+        """
 
-        qbtn = QPushButton('Quit')
+        self.setStyleSheet(in_button_style)
+
+        qbtn = QPushButton()
+        qbtn.setIcon(self.style().standardIcon(QStyle.SP_DialogCloseButton))
+        qbtn.setToolTip('Cancel')
         qbtn.clicked.connect(lambda: self.close())
+        qbtn.setMaximumWidth(35)
+        qbtn.setMaximumHeight(35)
+        self.maximizeButton.setMaximumHeight(35)
 
-        maximize = QPushButton('Maximize')
-        maximize.clicked.connect(self.makeFullScreen)
+        left = self.makeDirButton(Direction.left, QStyle.SP_ArrowBack, dir_button_style)
+        right = self.makeDirButton(Direction.right, QStyle.SP_ArrowForward, dir_button_style)
+        top = self.makeDirButton(Direction.top, QStyle.SP_ArrowUp, dir_button_style)
+        bottom = self.makeDirButton(Direction.bottom, QStyle.SP_ArrowDown, dir_button_style)
 
-        left = self.makeDirButton(Direction.left, QStyle.SP_ArrowBack, transp_button_style)
-        right = self.makeDirButton(Direction.right, QStyle.SP_ArrowForward, transp_button_style)
-        top = self.makeDirButton(Direction.top, QStyle.SP_ArrowUp, transp_button_style)
-        bottom = self.makeDirButton(Direction.bottom, QStyle.SP_ArrowDown, transp_button_style)
+        headerLt = QHBoxLayout()
+        headerLt.addWidget(self.maximizeButton)
+        headerLt.addWidget(qbtn)
+
+        centralLt = QVBoxLayout()
+        centralLt.addLayout(headerLt)
+        self.layoutEdits()
+        centralLt.addLayout(makeHCenterLayout(self.editWidget))
+        centralLt.addWidget(self.namedRegionsSelector)
+        centralLt.addWidget(self.takeScreenshot)
+        centralLt.addWidget(self.takeScreenshotDelayed)
 
         ltLeftRight = QHBoxLayout()
         ltLeftRight.setSpacing(0)
         ltLeftRight.setContentsMargins(0, 0, 0, 0)
         ltLeftRight.addWidget(left)
         ltLeftRight.addStretch(10)
-        ltLeftRight.addWidget(qbtn)
-        ltLeftRight.addWidget(maximize)
+        ltLeftRight.addLayout(centralLt)
         ltLeftRight.addStretch(10)
         ltLeftRight.addWidget(right)
+
 
         vertLt = QVBoxLayout()
         vertLt.setContentsMargins(0, 0, 0, 0)
@@ -258,18 +506,18 @@ class WindowRegionSelector(QtGui.QMainWindow):
         self.setContentsMargins(0, 0, 0, 0)
         self.setCentralWidget(mainWidget)
 
-        # self.setGeometry(0, 0, 1024, 768)
-        # self.setWindowTitle('Quit button')
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setWindowTitle('Screenshot selection')
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.show()
-
 
 
 def main():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ex = WindowRegionSelector()
+    ex.show()
+    ex.openSaveRegionDialog()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
